8ee9a398c31ef6797a24217145465ee28f624cdf
jimmu0410 <1922295863@qq.com>
2023-08-09 14:17:29 +0800

    sophon-schedule init commits

---
+++ sophon-schedule-api/src/main/java/com/sophon/schedule/api/SophonScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package com.sophon.schedule.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SophonScheduleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SophonScheduleApiApplication.class, args);
+    }
+
+}
